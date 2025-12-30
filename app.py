f06805a88d02511dba4dda824d070bf79bb4bde7
lekhrajprajapati <plekhraj838@gmail.com>
2025-12-30 23:35:46 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import numpy as np
+import pickle
+from tensorflow.keras.preprocessing.image import load_img
+from PIL import Image
+import matplotlib.pyplot as plt
+
+def load_model():
+    with open('model.pkl', 'rb') as file:
+        model = pickle.load(file)
+    return model
+
+
+def get_image_features(image):
+    img = load_img(image, color_mode = 'rgb')
+    img = img.resize((128,128) , Image.Resampling.LANCZOS)
+    img = np.array(img)
+    img = img.reshape(1,128,128,3)
+    img = img / 255.0
+    return img
+
+def main():
+    st.title("UTKFace Age and Gender Prediction")
+    st.write("Upload an image to predict the age and gender.")
+    # Allow user to upload image
+    img_to_test = st.file_uploader("Upload an Image", type=["jpg", "png", "jpeg"])
+    if img_to_test is not None:
+        # Preprocess image and predict results
+        processed_image = get_image_features(img_to_test)
+        
+        # Load the pre-trained model
+        model = load_model()
+        # Make prediction
+        pred = model.predict(processed_image)
+        # Map gender to appropriate labels
+        gender_mapping = {0: 'Male', 1: 'Female'}
+        gender = gender_mapping[round(pred[0][0][0])]  # Male or Female
+        age = round(pred[1][0][0])  # Predicted Age
+        # Show the result plot with the image
+        plt.title(f'Predicted Age: {age} Predicted Gender: {gender}')
+        plt.axis('off')
+        plt.imshow(np.array(load_img(img_to_test)))  # show the uploaded image
+        st.pyplot(plt)
+if __name__ == "__main__":
+    main()
